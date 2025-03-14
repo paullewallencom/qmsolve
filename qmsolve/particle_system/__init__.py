f815716be33f3f8fa18f5740453fbbaf426df93f
Paul Lewallen <paul.lewallen@gmail.com>
2025-03-13 17:06:40 -0700

    Fixing more circular import issues
    Fixing more circular import issues.

--- qmsolve/particle_system/__init__.py
+++ qmsolve/particle_system/__init__.py
@@ -18,3 +18,15 @@ def get_two_bosons():
 def get_two_particles():
     from .two_particles import TwoParticles
     return TwoParticles
+
+
+# Define a dynamic attribute to make ParticleSystem accessible in tests
+import sys
+import types
+
+module = sys.modules[__name__]
+module.ParticleSystem = get_particle_system()
+
+# Explicitly expose these in __all__
+__all__ = ["ParticleSystem", "get_single_particle",
+           "get_two_bosons", "get_two_particles"]
